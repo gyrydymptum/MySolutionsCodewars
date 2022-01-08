c2f22e98dba8d124ce57fe08690ea576af1ae115
Gyrydymptum <gyrydymptum@gmail.com>
2022-01-08 17:30:27 +0100

    format fix

--- findNextSquare/next_square.hpp
+++ findNextSquare/next_square.hpp
@@ -1,14 +1,11 @@
 #pragma once
-
 #include <cmath>
 #include "next_square.hpp"
 
-
 long int findNextSquare(long int sq) {
- long double number = std::sqrt(sq);
-  if (number == int(number)){
-    return (number + 1) * (number + 1);
-  } return -1;
+    long double number = std::sqrt(sq);
+    if (number == int(number)) {
+        return (number + 1) * (number + 1);
+    }
+    return -1;
 }
-  
- 
